8efd47090776aa57c63bdf71106afa3511185be6
A18D <A18D@bk.ru>
2018-09-19 21:20:36 +0300

    add sub menu

---
+++ src/components/routes/training/begin.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+
+export const Begin = () =>
+    <section>
+        <h2>С чего начать</h2>
+        <p>
+            Тесты
+        </p>
+        <p>
+            Задачи
+        </p>
+    </section>
