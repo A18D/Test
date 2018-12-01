0017ef675caba6dc1d4c877268035d9dbe25e5aa
A18D <A18D@bk.ru>
2018-12-01 18:23:14 +0300

    change details

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
-//import CSelect from './TestSelect'
 import Root from './components/Root'
 import "./fonts/glyphicons-halflings-regular.eot"
 import "./fonts/glyphicons-halflings-regular.svg"
@@ -15,15 +14,11 @@ import "./fonts//font-awesome/fontawesome-webfont.woff"
 import "./fonts//font-awesome/fontawesome-webfont.woff2"
 import "./fonts//font-awesome/FontAwesome.otf"
 
-
-
-
 import "./css/bootstrap.css"
 import "./css/styleFooter.css"
 import "./css/font-awesome.css"
 
 import "./css/subMenu.css"
-//import store from './store'
 import "./css/styleMenu.css"
 import "./css/LessonTemplate/styleMenuBegin.css"
 import "./css/LessonTemplate/styleRightLessonTemplates.css"
@@ -33,5 +28,3 @@ import "./css/LessonTemplate/styleCenterLessonTemplate.css"
 import "./css/LessonTemplate/styleTabTip.css"  
 
 render(<Root />, document.getElementById('react-container'))
-//render(<CSelect />, document.getElementById('react-container'))
-//render(<Store />, document.getElementById('react-container'))
